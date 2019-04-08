7e3f72dcf24021b159255a9dc8c486738c77a253
Your Name <your@email.com>
2019-04-08 23:04:51 +0000

    done

--- download_avatars.js
+++ download_avatars.js
@@ -10,20 +10,30 @@ function getRepoContributors(repoOwner, repoName, cb) {
         }
     };
 
+    function cb(data) {
+        for (i = 0; i < jSonArr.length; i++) {
+            console.log(jSonArr[i].avatar_url);
+        }
+    }
+
+
     request(options, function (err, res, body) {
+        // console.log(jSonArr);
+        // // for (var result in body) {
+        jSonArr = JSON.parse(body);
+
         cb(err, body);
+
     });
 }
 
 
-getRepoContributors("jquery", "jquery", function (err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-});
+
+getRepoContributors("jquery", "jquery", request);
 
 
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
 
 // curl - i - H 'Authorization: token 84a45c6cee4c46643a71d10dc0f86b5397e5faa4 https://api.github.com/repos/jquery/jquery/contributors
 // curl - u Jocelyne1604:84a45c6cee4c46643a71d10dc0f86b5397e5faa4 - I https://api.github.com/users/lighthouse-labs
