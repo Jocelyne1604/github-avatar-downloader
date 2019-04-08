a1fe21d58570fe1f98de7cb899487b8d93f46983
Your Name <your@email.com>
2019-04-08 21:50:30 +0000

    updated the token with secret.GITHUB_TOKEN from secret.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,29 @@
 var request = require('request');
+var secret = require('./secrets');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token ' + secret.GITHUB_TOKEN
+        }
+    };
+
+    request(options, function (err, res, body) {
+        cb(err, body);
+    });
+}
+
+
+getRepoContributors("jquery", "jquery", function (err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
+
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+// curl - i - H 'Authorization: token 84a45c6cee4c46643a71d10dc0f86b5397e5faa4 https://api.github.com/repos/jquery/jquery/contributors
+// curl - u Jocelyne1604:84a45c6cee4c46643a71d10dc0f86b5397e5faa4 - I https://api.github.com/users/lighthouse-labs
