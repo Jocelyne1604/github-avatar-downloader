4ba5799f9fd27af3460a552d9d9d1bd443ab8e8b
Your Name <your@email.com>
2019-04-09 14:18:29 +0000

    Images have downloaded successfully in the avatars folder + added some comments and changed token key that wasn't working

--- download_avatars.js
+++ download_avatars.js
@@ -1,39 +1,56 @@
 var request = require('request');
 var secret = require('./secrets');
+var fs = require('fs');
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
+
+if (process.argv.length < 4) {
+    throw "Repo owner required";
+}
+
+// domain name I  want to read from,
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
             'User-Agent': 'request',
+            //goes and get the token from my other secret.js file (token is confidential)
             'Authorization': 'token ' + secret.GITHUB_TOKEN
         }
     };
 
-    function cb(data) {
-        for (i = 0; i < jSonArr.length; i++) {
-            console.log(jSonArr[i].avatar_url);
-        }
-    }
-
-
     request(options, function (err, res, body) {
-        // console.log(jSonArr);
-        // // for (var result in body) {
         jSonArr = JSON.parse(body);
-
-        cb(err, body);
-
+        // [{avatar_url: ...}, {]}]
+        cb(err, jSonArr);
     });
 }
 
+function downloadImageByURL(url, filePath) {
+    //passed in Calback
+    request.get(url)
+        //2 options, if the link works, load the image and spit out a message, if not, throw error
+        .on('error', function (err) {
+            throw err;
+        })
+
+        .pipe(fs.createWriteStream(filePath))
+        .on('finish', function (response) {
+            console.log('image saved');
+        });
+}
+
+//Callback
+getRepoContributors("jquery", "jquery", function (err, result) {
+    //I want to get every avatar URL here SHOWS THE LIST:
+    for (i = 0; i < result.length; i++) {
+        var url = result[i].avatar_url;
+        var filePath = './avatars' + "/" + result[i].login + '.jpg';
+        //pass downloadImageByURL function what  link will the pipe follow to complete callback?
+        downloadImageByURL(url, filePath);
+    }
+});
 
 
-getRepoContributors("jquery", "jquery", request);
 
-
-
-
-
-// curl - i - H 'Authorization: token 84a45c6cee4c46643a71d10dc0f86b5397e5faa4 https://api.github.com/repos/jquery/jquery/contributors
-// curl - u Jocelyne1604:84a45c6cee4c46643a71d10dc0f86b5397e5faa4 - I https://api.github.com/users/lighthouse-labs
